ec3fee7fae49a5a350d6cd6368a19fb809d58678
resulib <mibadullayev98@gmail.com>
2024-09-08 10:47:20 +0400

    first commit

---
+++ src/main/java/com/resul/springgpt/SpringGptApplication.java
@@ -0,0 +1,13 @@
+package com.resul.springgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGptApplication.class, args);
+    }
+
+}
